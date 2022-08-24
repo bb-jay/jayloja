bfe72daebf6d73587fc6152774e42c6198917559
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-08-24 18:06:17 -0300

    primeiro commit

---
+++ src/main/java/br/com/sotuvieres/sotuviloja/SotuviLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sotuvieres.sotuviloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotuviLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotuviLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sotuvieres/sotuviloja/SotuviLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sotuvieres.sotuviloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotuviLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
